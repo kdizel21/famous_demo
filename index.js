927706d9e24ba106f21e55fc9e6ed77cbb4f78da
Kenny Davis <kenny.davis@amsoft.net>
2017-06-04 11:29:24 -0700

    trying to fix weird error with heroku

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ app.get('/', function (req, res) {
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('You are now listening on port 3000!')
 })
